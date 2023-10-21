f6efe2554a1392d45e5ecba29cbafda85c155ea8
minjun <ijmtylhtf@naver.com>
2023-10-21 01:17:22 -0700

    add comments

--- 4/1a-1.py
+++ 4/1a-1.py
@@ -1,22 +1,25 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-#RSA 생성
+#RSA 키쌍 생성
 keyPair = RSA.generate (2048)
 
-#개인키 
+#개인키 저장 
+#Key => Byte 암호화  
+#개인키이기 때문에, 비밀번호로 암호화 => 비밀번호를 알고 있는 사람만 사용 가능 
 priKeyPEM= keyPair.export_key (passphrase="1234")
-#print(priKeyPEM)
 
 with open('Alice_private.pem', 'wb') as output_file:
     output_file.write(priKeyPEM)
 
 
-#공개키 생성
+#공개키 분리 
 pubkey = keyPair.publickey( ) 
-#print(pubkey)
+
+#공개키 저장 
+#Key => Byte 
+#공개키이기 때문에, 비밀번호로 암호화 하지 않음 => 누구나 사용 가능 
 pubKeyPEM=pubkey.export_key()
-#print(pubKeyPEM)
 
 with open('Alice_public.pem', 'wb') as output_file:
     output_file.write(pubKeyPEM)
