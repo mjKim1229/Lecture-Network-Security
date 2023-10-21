408f70031bc7bd05ff6d19155ab428b0226191ba
minjun <ijmtylhtf@naver.com>
2023-10-21 01:34:12 -0700

    fix wrong variable name

--- 4/2b.py
+++ 4/2b.py
@@ -19,11 +19,15 @@ with open('sig.txt', 'rb') as file:
 # Base64 디코딩
 sigBytes = base64.b64decode(sigText)
 
+#1.txt 값 hash 
 h = SHA.new()
-h.update(msg)
+h.update(textData)
 
+#공개키로 검증 객체 생성 
 verifier = PKCS1_PSS.new(pubKey)
 
+#1.txt에서 hash한 값과 
+#sig.txt에서 읽은 1.txt를 전자서명한 값을 verify 
 if verifier.verify(h, sigBytes):
     print("The signature is authentic.")
 else:
